60e8ed357c7a0890823b2c6488ac31f0ec649d24
Davis Sylvester <davis@sylvesterllc.com>
2024-05-28 00:09:01 -0500

    Adding output

--- lambda-dynamo-s3-module/outputs.tf
+++ lambda-dynamo-s3-module/outputs.tf
@@ -1,1 +1,19 @@
+output "dynamo_table_name" {
+  value = aws_dynamodb_table.lds_database.name
+}
 
+output "dynamo_table_arn" {
+  value = aws_dynamodb_table.lds_database.id
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.base_artifacts_bucket.bucket
+}
+
+output "lambda_name" {
+  value = aws_lambda_function.aws_lds_lambda.function_name
+}
+
+output "lambda_arn" {
+  value = aws_lambda_function.aws_lds_lambda.arn
+}
